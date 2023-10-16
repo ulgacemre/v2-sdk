3866a9f0339d12a1641ea54e757a7a4105f83b4c
nithronium <45077058+nithronium@users.noreply.github.com>
2023-10-17 01:16:49 +0300

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -22,7 +22,7 @@ export const FACTORY_ADDRESS = '0xe683C94A31cf58D8eCbD642E4687257DA53142ea'
 
 export const INIT_CODE_HASH = '0x417eb21fb70cce70b5ecd83247eb73d233d19eb7568baed69c3e3214e36dd6d2'
 
-export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1)
 
 // exports for internal consumption
 export const ZERO = JSBI.BigInt(0)
